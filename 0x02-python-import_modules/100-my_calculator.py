e8bd050e50b6034c7a93a14e756e3e774b74d0cc
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-11 14:41:19 +0100

    If you can call this a calculator...

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 100-my_calculator.py
+# Olorundamisi Dunmade <github.com/olorundamisi>
+
+
+if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
+
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    op_dict = { "+" : add, "-" : sub, "*" : mul, "/" : div }
+    if sys.argv[2] not in list(op_dict.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    print("{} {} {} = {}".format(a, sys.argv[2], b, op_dict[sys.argv[2]](a, b)))
