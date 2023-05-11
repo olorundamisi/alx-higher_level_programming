70bfd71adc5fcd828722d324110e1749eec485c3
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-11 14:06:16 +0100

    Removed main() function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,16 +3,11 @@
 # Olorundamisi Dunmade <www.github.com/olorundamisi>
 
 
-def main():
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
-
-
-
 if __name__ == "__main__":
     """Print the sum of 1 and 2."""
 
     from add_0 import add
 
-    main()
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
