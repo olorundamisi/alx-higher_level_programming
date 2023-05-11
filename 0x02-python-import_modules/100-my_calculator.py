d185a6395efd1fc5c5a84d03a0583ff2f66c5231
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-11 15:12:43 +0100

    Dear ALX, can we please have the betty style guide for python???

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    op_dct = { "+" : add, "-" : sub, "*" : mul, "/" : div }
+    op_dct = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(op_dct.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
