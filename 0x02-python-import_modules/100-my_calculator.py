73b7fdbfad164a747941b16d072f9a11fd5fbd79
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-11 15:04:51 +0100

    Does this appease you betty...

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -21,4 +21,5 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    print("{} {} {} = {}".format(a, sys.argv[2], b, op_dict[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b,
+                                 op_dict[sys.argv[2]](a, b)))
