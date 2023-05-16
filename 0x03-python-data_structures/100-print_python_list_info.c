ad795a141144af56ca1e49ee041a6f0c5af32edd
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-16 02:10:05 +0100

    CPython #0: Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,35 @@
+/**
+ * File: 100-print_python_list_info.c
+ *
+ * Author: Olorundamisi Dunmade <github.com/olorundamisi>
+ */
+
+
+#include <Python.h>
+
+
+/**
+ * print_python_list_info - Print basic information about Python lists.
+ *
+ * @p: A PyObject list.
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	PyObject *object;
+
+	const int size = Py_SIZE(p);
+	const int alloced = ((PyListObject *)p)->allocated;
+	int i;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloced);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+
+		object = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(object)->tp_name);
+	}
+}
