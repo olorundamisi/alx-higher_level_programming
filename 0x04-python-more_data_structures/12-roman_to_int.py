73f28175269e457f94db97b8906a717926a64aa5
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-18 04:52:27 +0100

    Use neg

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -25,8 +25,8 @@ def roman_to_int(roman_string):
         if roman_dict.get(roman_string[i], 0) == 0:
             return (0)
 
-        if (i != (len(roman_string) - 1) and roman_dict[roman_string[i]] <
-            roman_dict[roman_string[i + 1]]):
+        neg = roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]])
+        if (i != (len(roman_string) - 1) and neg:
             arabic_int += roman_dict[roman_string[i]] * -1
         else:
             arabic_int += roman_dict[roman_string[i]]
