756a619436a8aa8c1d26e8c48bfd923eda919ef1
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-18 04:02:16 +0100

    Arabic numeral supremacy

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,11 +4,10 @@
 
 
 def roman_to_int(roman_string):
-    """Convert a roman numeral to an arabic integer."""
+    """Convert a roman numeral to an arabic numeral."""
 
-    roman_str = roman_string
-
-    if (not isinstance(roman_str, str) or roman_str is None):
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
         return (0)
 
     roman_dict = {
@@ -19,18 +18,17 @@ def roman_to_int(roman_string):
             "C": 100,
             "D": 500,
             "M": 1000
-            }
+    }
+    arabic_num = 0
 
-    arabic_int = 0
-
-    for i in range(len(roman_str)):
-        if roman_dict.get(roman_str[i], 0) == 0:
+    for i in range(len(roman_string)):
+        if roman_dict.get(roman_string[i], 0) == 0:
             return (0)
 
-        if (i != (len(roman_str) - 1) and
-                roman_dict[roman_str[i]] < roman_dict[roman_str[i + 1]]):
-                arabic_int += roman_dict[roman_str[i]] * -1
-        else:
-            arabic_int += roman_dict[roman_str[i]]
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+                arabic_num += roman_dict[roman_string[i]] * -1
 
-    return (arabic_int)
+        else:
+            arabic_num += roman_dict[roman_string[i]]
+    return (arabic_num)
