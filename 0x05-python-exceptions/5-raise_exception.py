51131bc8bf1eb31dc36460e9f065720205ebffe5
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-29 14:59:02 +0100

    Raise Exceptions

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-raise_exception.py
+# Olorundamisi Dunmade <github.com/olorundamisi>
+
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Olorundamisi Dunmade <github.com/olorundamisi>
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
