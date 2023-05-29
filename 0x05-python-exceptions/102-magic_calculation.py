b7289b81e6d1f92ec8c9262c699208c2964b43d4
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-29 21:26:18 +0100

    vE: Reverse Engineering Python ByteCode and Raising Exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
-# Olorundamisi Dunmade <github.com/olorundamisi>
 
 
 def magic_calculation(a, b):
@@ -8,9 +6,9 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception("Too far")
+                raise Exception('Too far')
             else:
-                result += ((a ** b) / i)
+                result += a ** b / i
         except:
             result = b + a
             break
