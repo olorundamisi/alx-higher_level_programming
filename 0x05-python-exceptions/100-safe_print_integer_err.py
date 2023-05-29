688e6cc78f65451dc986d086c75bf2ce3408916c
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-05-29 14:40:22 +0100

    Catching multiple Errors in Python

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# 100-safe_print_integer_err.py
+# Olorundamisi Dunmade <github.com/olorundamisi>
+
+import sys
+
+def safe_print_integer_err(value):
+    """Print an integer with "{:d}".format().
+
+    If an Error message is caught, the corresponding error message is printed
+    to standard error.
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# 101-safe_function.py
+# Olorundamisi Dunmade <github.com/olorundamisi>
+
+import sys
+
+def safe_function(fct, *args):
+    """Execute a function safely.
+
+    Args:
+        fct: The function to execute.
+        args: Arguments for the function `fct`.
+
+    Returns:
+        If an Error occurs - None.
+        Otherwise - the result of the call to fct.
+    """
+    try:
+        result = fct(*args)
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
