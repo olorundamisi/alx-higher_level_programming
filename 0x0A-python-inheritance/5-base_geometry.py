c3eeb3bbffd48fe21c652234db870ffd5feb7cbb
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-06-12 22:36:12 +0100

    Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Olorundamisi Dunmade <github.com/olorundamisi>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
