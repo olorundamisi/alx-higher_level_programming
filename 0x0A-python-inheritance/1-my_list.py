23a61709e178f2499a243ea002176c17712a4980
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-06-13 01:11:18 +0100

    0111 : if not hasattr(obj, __dict__):; raise TypeError(...); setattr(...);

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -10,3 +10,8 @@ class MyList(list):
     def print_sorted(self):
         """Print a list sorted in non-decreasing/ascending order."""
         print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
