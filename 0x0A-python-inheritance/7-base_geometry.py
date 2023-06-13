ab850df683e485eb3f50a48ea174fa2b30e7faa5
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-06-13 02:06:17 +0100

    0206: if not hasattr(obj, __dict__):; raise TypeError(...); setattr(...);

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -23,10 +23,5 @@ class BaseGeometry:
         """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/7-base_geometry.txt")
