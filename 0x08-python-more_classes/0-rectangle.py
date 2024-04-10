85e533b014c00c01d7b38577c0a43c34a58e6d2d
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2024-04-10 04:52:06 +0100

    Empty class that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Olorundamisi Dunmade <github.com/olorundamisi>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
