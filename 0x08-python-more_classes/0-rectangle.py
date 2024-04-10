a991e43697546c6e444eee8569c01b8047ea1def
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2024-04-10 04:46:44 +0100

    Remove files

--- 0x08-python-more_classes/0-rectangle.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# 0-rectangle.py
-# Olorundamisi Dunmade <github.com/olorundamisi>
-"""Defines a Rectangle class."""
-
-
-class Rectangle:
-    """Represent a rectangle."""
-    pass
