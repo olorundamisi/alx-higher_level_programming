fe39f4b966e9b16c722c6312ebf47473b18ce195
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2024-04-16 03:06:48 +0100

    Add files for 0x0A. Python - Inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -14,4 +14,4 @@ def is_same_class(obj, a_class):
         If obj is exactly an instance of a_class - True.
         Otherwise - False.
     """
-    return type(obj) == a_class
+    return type(obj) is a_class
